489ee6f89b4ab6838e5acd57368d478c4721c301
Eric Meldrum <eric.meldrum@gmail.com>
2019-10-20 12:42:12 -0600

    Add the stuff to do the things

--- src/index.js
+++ src/index.js
@@ -1,13 +1,75 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
-import $ from 'jquery';
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+let rankedFruits = [];
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const submitBtn = document.querySelector('#submit')
+const main = document.querySelector('main')
+const fruitList = document.querySelector('#fruit-list')
+const tasteScore = document.querySelector('#taste-num')
+const consScore = document.querySelector('#cons-num')
+const versScore = document.querySelector('#vers-num')
+const portScore = document.querySelector('#port-num')
+const longScore = document.querySelector('#long-num')
+
+const submitFruit = (e) => {
+  e.preventDefault();
+  if (fruitList.value && tasteScore.value && consScore.value && versScore.value && portScore.value && longScore.value) {
+    createNewFruit();
+  }
+  clearMain();
+  displayFruits();
+  clearInputs();
+}
+
+const clearMain = () => {
+  main.innerHTML = "";
+}
+
+const clearInputs = () => {
+  fruitList.selectedIndex = 0;
+  tasteScore.value = "";
+  consScore.value = "";
+  versScore.value = "";
+  portScore.value = "";
+  longScore.value = "";
+}
+
+const createNewFruit = () => {
+  let newFruit = new Fruit(fruitList.value, tasteScore.value, consScore.value, versScore.value, portScore.value, longScore.value);
+  // debugger;
+  rankedFruits.push(newFruit);
+  rankedFruits.sort((fruitA, fruitB) => fruitB.avg - fruitA.avg);
+  console.log(rankedFruits);
+  return newFruit;
+}
+
+const displayFruits = () => {
+  rankedFruits.forEach((fruit, i) => {
+    main.insertAdjacentHTML("beforeend", 
+    `<div class="card">
+      <h2>${fruit.name}</h2>
+      <h3>Peak taste: ${fruit.taste}</h3>
+      <h3>Consistency: ${fruit.consistency}</h3>
+      <h3>Versatility: ${fruit.versatility}</h3>
+      <h3>Portability: ${fruit.portability}</h3>
+      <h3>Longevity: ${fruit.longevity}</h3>
+      <h3>Average: ${fruit.avg}</h3>
+      <h2>Rank: ${i + 1}
+    </div>`
+  )
+  })
+}
+
+class Fruit {
+  constructor(name, taste, consistency, versatility, portability, longevity) {
+    this.name = name;
+    this.taste = parseInt(taste);
+    this.consistency = parseInt(consistency);
+    this.versatility = parseInt(versatility);
+    this.portability = parseInt(portability);
+    this.longevity = parseInt(longevity);
+    this.avg = (this.taste + this.consistency + this.versatility + this.portability + this.longevity) / 5;
+  }
+}
+
+submitBtn.addEventListener('click', submitFruit)
